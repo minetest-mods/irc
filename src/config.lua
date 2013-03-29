04a7414179b708aae8c071ec10cb95e7880f552e
0gb.us <0gb.us@0gb.us>
2013-03-28 22:18:14 -0700

    Move settings to minetest.conf
    Default settings and the places they are defined have not been changed.

--- src/config.lua
+++ src/config.lua
@@ -1,4 +1,3 @@
-
 -- IRC Mod for Minetest
 -- By Diego Martínez <kaeza@users.sf.net>
 --
@@ -16,46 +15,46 @@
 -- *************************
 
 -- Server to connect on joinplayer (string, default "irc.freenode.net")
-mt_irc.server = nil;
+mt_irc.server = minetest.setting_get("mt_irc.server");
 
 -- Port to connect on joinplayer (number, default 6667)
-mt_irc.port = nil;
+mt_irc.port = tonumber(minetest.setting_get("mt_irc.port"));
 
 -- Channel to connect on joinplayer (string, default "##mt-irc-mod")
-mt_irc.channel = nil;
+mt_irc.channel = minetest.setting_get("mt_irc.channel");
 
 -- ***********************
 -- ** ADVANCED SETTINGS **
 -- ***********************
 
 -- Time between chat updates in seconds (number, default 0.2).
-mt_irc.dtime = nil;
+mt_irc.dtime = tonumber(minetest.setting_get("mt_irc.dtime"));
 
 -- Underlying socket timeout in seconds (number, default 1.0).
-mt_irc.timeout = nil;
+mt_irc.timeout = tonumber(minetest.setting_get("mt_irc.timeout"));
 
 -- Nickname when using single conection (string, default "minetest-"..<server-id>);
 --  (<server-id> is the server IP address packed as a 32 bit integer).
-mt_irc.server_nick = nil;
+mt_irc.server_nick = minetest.setting_get("mt_irc.server_nick");
 
 -- Password to use when using single connection (string, default "")
-mt_irc.password = nil;
+mt_irc.password = minetest.setting_get("mt_irc.password");
 
 -- The format of messages sent to IRC server (string, default "<$(name)> $(message)")
 -- See `README.txt' for the macros supported here.
-mt_irc.message_format_out = "<$(name)> $(message)";
+mt_irc.message_format_out = minetest.setting_get("mt_irc.message_format_out") or "<$(name)> $(message)";
 
 -- The format of messages sent to IRC server (string, default "<$(name)@IRC> $(message)")
 -- See `README.txt' for the macros supported here.
-mt_irc.message_format_in = "<$(name)@IRC> $(message)";
+mt_irc.message_format_in = minetest.setting_get("mt_irc.message_format_in") or "<$(name)@IRC> $(message)";
 
 -- Enable debug output (boolean, default false)
-mt_irc.debug = true;
+mt_irc.debug = not minetest.setting_get_bool("mt_irc.disable_debug");
 
 -- Whether to automatically join the channed when player joins
 --  (boolean, default true)
-mt_irc.auto_join = true;
+mt_irc.auto_join = not minetest.setting_get_bool("mt_irc.disable_auto_join");
 
 -- Whether to automatically connect to the server on mod load
 --  (boolean, default true) 
-mt_irc.auto_connect = true;
+mt_irc.auto_connect = not minetest.setting_get_bool("mt_irc.disable_auto_connect");
